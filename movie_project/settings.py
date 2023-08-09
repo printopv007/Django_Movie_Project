e219f49daf538e727528328c1837e38b5a128708
Printo P Varghese <printopv007@gmail.com>
2023-08-09 20:00:08 +0530

    deploy

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-76h7q5(6irg2ytv-_fa#d4pikjx_%x+=dqzw2)%n4jngsv5m*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
